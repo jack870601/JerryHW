6563f743b18f1dfb71d778dab680c5b3af36daa6
jack870601 <jack870601@gmail.com>
2018-03-26 21:43:57 +0800

    0326

---
+++ 0326/main.py
@@ -0,0 +1,6 @@
+from sources import calculate
+
+print("calculate:2+3",calculate._calculate(2,3,'+'))
+print("calculate:7-4",calculate._calculate(7,4,'-'))
+print("calculate:5*6",calculate._calculate(5,6,'*'))
+print("calculate:8/4",calculate._calculate(8,4,'/'))
---
+++ 0326/sources/calculate.py
@@ -0,0 +1,9 @@
+def _calculate(x,y,operator):
+    if(operator == '+'):
+        return (x + y)
+    if(operator == '-'):
+        return (x - y)
+    if(operator == '*'):
+        return (x * y)
+    if(operator == '/'):
+        return (x / y)
